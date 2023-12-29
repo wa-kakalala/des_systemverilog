7bfe03d0a35b151dd012072ad382f92f42b3578f
wangchaosun <2962056732@qq.com>
2023-12-29 21:23:11 +0800

    first commit

---
+++ shell_tools/generate_key/generate_key.py
@@ -0,0 +1,25 @@
+def get_varinfo(config_path:str)->[str,[]]:
+    f = open(config_path,"r")
+    varname = f.readline().strip()
+    varidxs = []
+    for line in f.readlines():
+        for idx in line.split():
+            varidxs.append(int(idx.strip()))
+    f.close()
+    return [varname,varidxs]
+def output_expression(varname:str,varidxs:[int],linenum=8,databit=2)->None:
+    print("{")
+    for idx in range(len(varidxs)-1):
+        print(("{}[{:"+str(databit)+"}],").format(varname,varidxs[idx]),end=' ')
+        if (idx+1)%linenum == 0 :
+            print("")
+    print(("{}[{:"+str(databit)+"}]").format(varname,varidxs[len(varidxs)-1]))
+    print("};")
+        
+
+def main():
+    varname , varidxs = get_varinfo("./config.txt")
+    output_expression(varname,varidxs)
+
+if __name__ == "__main__":
+    main()
